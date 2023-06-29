d80027afc8de178b6fe4fbdcf9558bd42adedfdd
Chandramol Pandey <pandeychandramol@gmail.com>
2023-06-30 02:38:59 +0530

    Task in vue js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!--Here, This data is coming from home.vue component as such import -->
+    <Home data="Yes you are here and also started work with vue" msg="You must need to focus with vue"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    // It is also we have defined when we create a new component
+    Home
   }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<!-- There is two part of new component one is template and second is script -->
+<template>
+    <!-- in template there is only one route component -->
+    <div>
+        <h1>Data coming from Home</h1>
+        <p>{{ data }}</p>
+        <p>{{ msg }}</p>
+        <button v-on:click="myFun()">Call Me</button>
+        <!-- if else condition -->
+        <h2 v-if="show">Show this line on vue</h2>
+        <h2 v-else>Hide this line for vue</h2>
+        <button v-on:click="toggle()">Toggle</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'Home',
+        // It is compulsory to define data type of property when we are using interpolation as below
+        props:{
+            data:String,
+            msg:String
+        },
+        // For event Element we have to use of method
+        methods:{
+            myFun(){
+                alert("Hello chanmol, what's going with vue ?")
+            },
+            // To make toggle for if else condition
+            toggle(){
+                if(this.show==true)
+                this.show=false
+                else{
+                    this.show=true
+                }
+                // this.show=!this.show         //This is known as negation which work short of if else condition
+            }
+        },
+        // For making toggle we have to make data() likewise
+        data(){
+            return {show:true}
+        }
+    }
+</script>
